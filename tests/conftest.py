54eb11659ed588bc88ce60cfc129f16d5a9f646f
Colin Smith <colin.smith@wisc.edu>
2025-03-10 07:01:20 -0700

    tests: fix untested scenarios, and default to mocking

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ from geoenvo.response import Response, compile_response
 
 @pytest.fixture()
 def use_mock():
-    return False  # Change this to False for real HTTP requests and data
+    return True  # Change this to False for real HTTP requests and data
 
 
 @pytest.fixture
@@ -36,26 +36,26 @@ def scenarios(
     properties_of_world_terrestrial_ecosystems,
 ):
     scenarios = [
-        # {  # WTE Success (Envelop on land)
-        #     "data_source": WorldTerrestrialEcosystems(),
-        #     "response": load_response("wte_success"),
-        #     "geometry": load_geometry("point_on_land"),
-        #     "unique_environment": 1,
-        #     "has_environment": True,
-        #     "raw_properties": raw_properties_of_world_terrestrial_ecosystems,
-        #     "properties": properties_of_world_terrestrial_ecosystems,
-        #     "identifier": "https://doi.org/10.5066/P9DO61LP",
-        # },
-        # {  # WTE Fail (A point over the ocean)
-        #     "data_source": WorldTerrestrialEcosystems(),
-        #     "response": load_response("wte_fail"),
-        #     "geometry": load_geometry("point_on_ocean"),
-        #     "unique_environment": 0,
-        #     "has_environment": False,
-        #     "raw_properties": raw_properties_of_world_terrestrial_ecosystems,
-        #     "properties": properties_of_world_terrestrial_ecosystems,
-        #     "identifier": "https://doi.org/10.5066/P9DO61LP",
-        # },
+        {  # WTE Success (Envelop on land)
+            "data_source": WorldTerrestrialEcosystems(),
+            "response": load_response("wte_success"),
+            "geometry": load_geometry("point_on_land"),
+            "unique_environment": 1,
+            "has_environment": True,
+            "raw_properties": raw_properties_of_world_terrestrial_ecosystems,
+            "properties": properties_of_world_terrestrial_ecosystems,
+            "identifier": "https://doi.org/10.5066/P9DO61LP",
+        },
+        {  # WTE Fail (A point over the ocean)
+            "data_source": WorldTerrestrialEcosystems(),
+            "response": load_response("wte_fail"),
+            "geometry": load_geometry("point_on_ocean"),
+            "unique_environment": 0,
+            "has_environment": False,
+            "raw_properties": raw_properties_of_world_terrestrial_ecosystems,
+            "properties": properties_of_world_terrestrial_ecosystems,
+            "identifier": "https://doi.org/10.5066/P9DO61LP",
+        },
         {  # ECU Success (Envelop spanning coastal area)
             "data_source": EcologicalCoastalUnits(),
             "response": load_response("ecu_success"),
